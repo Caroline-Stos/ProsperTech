4ed210e0d88d699666acfc4ee0670d9c879829db
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-08-17 20:07:56 -0300

    exercicio geladeira ok

---
+++ MeuPrimeiroProjeto/Geladeira_Ex/Andar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto.Geladeira_Ex
+{
+    internal class Andar
+    {
+        // propriedades
+        public List<Container> ContainerList { get; set; } = new List<Container>();
+
+        public Andar()
+        {
+            // Inicializa com dois containers por padrão
+            ContainerList.Add(new Container());
+            ContainerList.Add(new Container());
+        }
+
+
+    }
+}
---
+++ MeuPrimeiroProjeto/Geladeira_Ex/Container.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuPrimeiroProjeto.Geladeira_Ex
+{
+    internal class Container
+    {
+        //propriedades
+        public List<string> ItensList { get; set; } = new List<string>(new string[4]);
+
+        //metodos
+        public void AddItem(int indice, string item)
+        {
+            if (string.IsNullOrEmpty(ItensList[indice]))
+            {
+                if (ItensList.Contains(item))
+                {
+                    Console.WriteLine($"{item} já existe no container.");
+                }
+                else
+                {
+                    ItensList[indice] = item;
+                    Console.WriteLine($"{item} adicionado com sucesso.");
+                }
+            }
+            else { Console.WriteLine("Já existe um produto nesta posição."); }
+        }
+        public void RemoverItem(string item)
+        {
+            ItensList.Remove(item);
+            Console.WriteLine($"{item} removido com sucesso.");
+        }
+        public void LimparContainer()
+        {
+            if (ItensList.Count > 0)
+            {
+                ItensList.Clear();
+                Console.WriteLine("O container foi esvaziado.");
+            }
+            else
+            {
+                Console.WriteLine("O container está vazio.");
+            }
+        }
+        public List<string> ListarItens()
+        {
+            ItensList = ItensList.Where(i => !string.IsNullOrEmpty(i)).ToList();
+            foreach (string item in ItensList)
+            {
+                Console.WriteLine(item + ", ");
+            }
+            return ItensList;
+        }
+        public void StatusContainer()
+        {
+            var itensPreenchidos = ItensList.Count(i => !string.IsNullOrEmpty(i));
+            if (itensPreenchidos == 4)
+            {
+                Console.WriteLine("O container está cheio.");
+            }
+            else if (itensPreenchidos == 0)
+            {
+                Console.WriteLine("O container está vazio.");
+            }
+            else
+            {
+                var posicoesVazias = 4 - itensPreenchidos;
+                Console.WriteLine($"O container tem {posicoesVazias} lugar(es) vazio(s).");
+            }
+        }
+    }
+}
