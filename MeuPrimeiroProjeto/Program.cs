f21d9aafe99af3547303845c96d06a1e947c681b
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-08-19 17:37:15 -0300

    ajustando arquivos

--- MeuPrimeiroProjeto/Program.cs
+++ MeuPrimeiroProjeto/Program.cs
@@ -1,7 +1,6 @@
 ﻿using MeuPrimeiroProjeto.Aula1_1;
 using MeuPrimeiroProjeto.Aula1;
 using MeuPrimeiroProjeto.Aula2;
-using MeuPrimeiroProjeto.Aula_5;
 using MeuPrimeiroProjeto.Geladeira_Ex;
 using System.Text;
 
@@ -21,41 +20,4 @@ using System.Text;
 
 // Aula 6 - Versionamento GIT (teorico);
 
-// Geladeira Parte 2
 
-// testando Container -----------------------------------------
-// Container NovoContainer = new Container();
-// NovoContainer.AddItem(0, "Maça");
-// NovoContainer.ListarItens();
-// NovoContainer.StatusContainer();
-// NovoContainer.RemoverItem("Maça");
-// NovoContainer.LimparContainer();
-
-// testando Andar -----------------------------------------
-
-//Andar HortiFruit =  new Andar();
-
-//Console.WriteLine("Adicionando itens ao primeiro container:");
-//HortiFruit.ContainerList[0].AddItem(0, "Maçã");
-//HortiFruit.ContainerList[0].AddItem(1, "Laranja");
-//HortiFruit.ContainerList[0].AddItem(2, "Melão");
-//HortiFruit.ContainerList[0].AddItem(3, "Uva");
-
-//HortiFruit.ContainerList[0].ListarItens();
-
-// testando Geladeira -----------------------------------------
-
-Geladeira_Poo minhaGeladeira = new Geladeira_Poo();
-
-// Adicionando itens aos andares
-minhaGeladeira.CarneAndar.ContainerList[0].AddItem(0, "Bife");
-minhaGeladeira.CarneAndar.ContainerList[1].AddItem(0, "Frango");
-minhaGeladeira.LaticAndar.ContainerList[0].AddItem(0, "Queijo");
-minhaGeladeira.FruitAndar.ContainerList[0].AddItem(0, "Maçã");
-minhaGeladeira.FruitAndar.ContainerList[1].AddItem(0, "Banana");
-
-//minhaGeladeira.VerGeladeira();
-
-minhaGeladeira.CarneAndar.ContainerList[0].ListarItens();
-minhaGeladeira.LaticAndar.ContainerList[0].ListarItens();
-minhaGeladeira.FruitAndar.ContainerList[0].ListarItens();
