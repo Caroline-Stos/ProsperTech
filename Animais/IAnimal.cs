f7985ce1ee0b689135c2936f1a1760c4937e3e91
Caroline-Stos <aluno103.23187208caroline@gmail.com>
2024-08-19 22:00:28 -0300

    Exercicio animais ok

---
+++ Animais/IAnimal.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Animais
+{
+    internal interface IAnimal
+    {
+        // propriedades
+        public bool Olhos { get; set; }
+        public bool Ouvidos { get; set; }
+        public bool Nariz { get; set; }
+        public bool Boca { get; set; }
+
+        //metodos
+        string FazerBarulho();
+        string Cheirar();
+        string Comer();
+        string Dormir();
+        string Correr();
+
+    }
+}
